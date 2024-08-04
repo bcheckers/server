169ad89d4ff5d3af0056bc936362f69bdebd4d55
Ben Daws <ben@linuxfornoobs.org>
2024-08-04 17:31:24 -0500

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("checkers_server")]
+[assembly: AssemblyDescription("Server DLL for building Checkers servers")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("btdaws")]
+[assembly: AssemblyProduct("Ben's Checkers")]
+[assembly: AssemblyCopyright("Copyright © Ben's Checkers 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("37b7e02f-681b-41df-a4cb-2ce75fcb777f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
